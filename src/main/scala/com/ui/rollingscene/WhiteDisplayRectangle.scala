9c81663e96b2b02d1747e14df6f5fd4d34283e63
Martin Ahmadzadeh <martin.ahmadzadeh@dominionmarinemedia.com>
2015-04-08 23:23:46 -0700

    haha love this effect. randomly assign a shade of green to each green display rectangle.

--- src/main/scala/com/ui/rollingscene/WhiteDisplayRectangle.scala
+++ src/main/scala/com/ui/rollingscene/WhiteDisplayRectangle.scala
@@ -2,7 +2,7 @@ package com.ui.rollingscene
 
 import java.awt.{Point, Color}
 import java.awt.Color._
-import com.util.random.RandomBoolean
+import com.util.random.{RandomNumberGenerator, RandomBoolean}
 
 case class WhiteDisplayRectangle(x: Int, y: Int, width: Int, height: Int)
     extends DisplayRectangle(x, y, width, height) {
@@ -12,7 +12,7 @@ case class WhiteDisplayRectangle(x: Int, y: Int, width: Int, height: Int)
 }
 case class GreenDisplayRectangle(x: Int, y: Int, width: Int, height: Int)
     extends DisplayRectangle(x, y, width, height) {
-    override val color: Color =  new Color (0, 128, 0)
+    override val color: Color =  new Color (0, new RandomNumberGenerator().next(10 to 246), 0)
 
     def moveTo(loc:Point):GreenDisplayRectangle = GreenDisplayRectangle(x + ( loc.x - x) , y, width, height)
 }
