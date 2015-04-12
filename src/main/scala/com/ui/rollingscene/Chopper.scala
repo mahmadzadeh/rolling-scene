5c9f42977b903ac6af8f0c1acb7c2b056195ed9c
Martin Ahmadzadeh <martin.ahmadzadeh@dominionmarinemedia.com>
2015-04-12 14:22:38 -0700

    getting a bounding box for the flying chopper

--- src/main/scala/com/ui/rollingscene/Chopper.scala
+++ src/main/scala/com/ui/rollingscene/Chopper.scala
@@ -1,19 +1,20 @@
 package com.ui.rollingscene
 
-import java.awt.{Graphics, Point}
+import java.awt.{Rectangle, Graphics, Point}
 import scala.util.Try
 import javax.imageio.ImageIO
+
+
+//abstract class Chopper
 
 class Chopper(topLeft: Point, velocity: Velocity) {
     val image       = Try {
         ImageIO.read(getClass.getResourceAsStream("/cobra_2.jpg"))
-    }.getOrElse(throw new IllegalStateException("Unable to load choppler image"))
-
-    val xs      = List[Int]()
-    val ys      = List[Int]()
+    }.getOrElse(throw new IllegalStateException("Unable to load chopper image"))
 
     def move:Chopper    = new Chopper(new Point(velocity.x1(topLeft.x), velocity.y1(topLeft.y)), velocity)
 
     def draw(g: Graphics): Unit = g.drawImage(image,topLeft.x, topLeft.y, null )
 
+    def boundingBox:Rectangle = new Rectangle(topLeft.x, topLeft.y, image.getWidth, image.getHeight)
 }
--- src/test/scala/com/ui/rollingscene/ChopperTest.scala
+++ src/test/scala/com/ui/rollingscene/ChopperTest.scala
@@ -4,11 +4,22 @@ import org.scalatest.FunSuite
 import java.awt.Point
 
 class ChopperTest extends FunSuite {
+    val chopperVelocity   = ChopperVelocity(1, 1)
+    val point              = new Point
 
     test("given point and velocity then a chopper can be created") {
-        val chpr = new Chopper(new Point, ChopperVelocity(1,1) )
+        val chpr = new Chopper(point, chopperVelocity )
     }
 
+    test("given a chopper then boudingbox returns the bounding box around the chopper") {
 
+        val chpr = new Chopper(point,chopperVelocity )
+
+        val bbox = chpr.boundingBox
+
+        assert(point.x === bbox.x)
+        assert(point.y === bbox.y)
+        assert(chpr.image.getWidth === bbox.width)
+    }
 
 }
